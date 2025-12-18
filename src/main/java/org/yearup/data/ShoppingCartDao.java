fd74d36ed9067303e667ab68ebb290cfade451be
Zain Ashraf <zainashraf1562@gmail.com>
2025-12-18 12:49:19 -0500

    started phase 3, finished interface for shoppingcartdao

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -5,5 +5,10 @@ import org.yearup.models.ShoppingCart;
 public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
+    ShoppingCart addToCart(int userId, int productId);
+    ShoppingCart updateCart(int userId, int productId, int quantity);
+    void deleteCart(int userId);
+
     // add additional method signatures here
+
 }

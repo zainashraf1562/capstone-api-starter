59610c5705ca0af9f7201a4d65c4d77a843174c4
Zain Ashraf <zainashraf1562@gmail.com>
2025-12-18 11:19:53 -0500

    getAll in CategoryDao finished. and passed Testing!

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,8 +1,7 @@
 package org.yearup.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
@@ -14,6 +13,10 @@ import java.util.List;
 // add the annotation to make this controller the endpoint for the following url
     // http://localhost:8080/categories
 // add annotation to allow cross site origin requests
+
+@RestController
+@RequestMapping("/categories")
+@CrossOrigin
 public class CategoriesController
 {
     private CategoryDao categoryDao;
@@ -21,18 +24,21 @@ public class CategoriesController
 
 
     // create an Autowired controller to inject the categoryDao and ProductDao
-
-    // add the appropriate annotation for a get action
-    public List<Category> getAll()
-    {
-        // find and return all categories
-        return null;
+    @Autowired
+    public CategoriesController(CategoryDao categoryDao){
+        this.categoryDao = categoryDao;
     }
 
     // add the appropriate annotation for a get action
+    @GetMapping()
+    public List<Category> getAll()
+    {
+        return categoryDao.getAllCategories();
+    }
+
+
     public Category getById(@PathVariable int id)
     {
-        // get the category by id
         return null;
     }
 

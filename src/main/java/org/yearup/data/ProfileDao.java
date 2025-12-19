c711ba91ef6bf6a507c304927d10d3a6f5c9aed2
Zain Ashraf <zainashraf1562@gmail.com>
2025-12-18 20:28:57 -0500

    added getbyuserid and update in ProfileDao

--- src/main/java/org/yearup/data/ProfileDao.java
+++ src/main/java/org/yearup/data/ProfileDao.java
@@ -6,4 +6,9 @@ import org.yearup.models.Profile;
 public interface ProfileDao
 {
     Profile create(Profile profile);
+
+    Profile getProfileByUserId(int userId);
+
+    void updateProfile(int userId, Profile profile);
+
 }

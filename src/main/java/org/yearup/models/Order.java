7c83b5388b491be262153af1691e3b495e40ce17
Zain Ashraf <zainashraf1562@gmail.com>
2025-12-18 21:13:18 -0500

    created models Order and OrderLineItem

--- src/main/java/org/yearup/models/Order.java
+++ src/main/java/org/yearup/models/Order.java
@@ -1,4 +1,92 @@
 package org.yearup.models;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class Order {
+    private int orderId;
+    private int userId;
+    private LocalDateTime date;
+    private String address;
+    private String city;
+    private String state;
+    private String zipCode;
+    private BigDecimal shippingAmount;
+
+    public Order(int orderId, int userId, LocalDateTime date, String address, String city, String state, String zipCode, BigDecimal shippingAmount) {
+        this.orderId = orderId;
+        this.userId = userId;
+        this.date = date;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.shippingAmount = shippingAmount;
+    }
+
+    public Order(){}
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public BigDecimal getShippingAmount() {
+        return shippingAmount;
+    }
+
+    public void setShippingAmount(BigDecimal shippingAmount) {
+        this.shippingAmount = shippingAmount;
+    }
 }
--- src/main/java/org/yearup/models/OrderLineItem.java
+++ src/main/java/org/yearup/models/OrderLineItem.java
@@ -1,4 +1,72 @@
 package org.yearup.models;
 
+import java.math.BigDecimal;
+
 public class OrderLineItem {
+    private int orderLineIdItem;
+    private int orderId;
+    private int productId;
+    private BigDecimal salesPrice;
+    private int quantity;
+    private BigDecimal discount;
+
+    public OrderLineItem() {
+    }
+
+    public OrderLineItem(int orderLineIdItem, int orderId, int productId, BigDecimal salesPrice, int quantity, BigDecimal discount) {
+        this.orderLineIdItem = orderLineIdItem;
+        this.orderId = orderId;
+        this.productId = productId;
+        this.salesPrice = salesPrice;
+        this.quantity = quantity;
+        this.discount = discount;
+    }
+
+    public int getOrderLineIdItem() {
+        return orderLineIdItem;
+    }
+
+    public void setOrderLineIdItem(int orderLineIdItem) {
+        this.orderLineIdItem = orderLineIdItem;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public BigDecimal getSalesPrice() {
+        return salesPrice;
+    }
+
+    public void setSalesPrice(BigDecimal salesPrice) {
+        this.salesPrice = salesPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(BigDecimal discount) {
+        this.discount = discount;
+    }
 }
